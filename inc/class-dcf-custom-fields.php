d5c4d9e294b269aacea7ef8d22fefb15ae41affd
Merhawi Fissehaye <merhawifissehaye@gmail.com>
2017-07-07 08:32:48 +0300

    Changed attributes

--- inc/class-dcf-custom-fields.php
+++ inc/class-dcf-custom-fields.php
@@ -8,7 +8,7 @@ class DcfCustomFields {
 
 	private static function register_video_custom_fields() {
 		add_filter( 'rwmb_meta_boxes', function ( $meta_boxes ) {
-			$prefix = 'dcf';
+			$prefix = 'dcf-';
 
 			$meta_boxes[] = array(
 				'id'       => 'youtube_embed_code',
@@ -22,6 +22,12 @@ class DcfCustomFields {
 						'id'   => $prefix . 'youtube_embed_code',
 						'type' => 'textarea',
 						'name' => esc_html__( 'Youtube Embed Code', 'dcf' ),
+					),
+					array(
+						'id' => $prefix . 'show_in_about_page',
+						'name' => esc_html__( 'Show in About Page', 'dcf' ),
+						'type' => 'checkbox',
+						'desc' => esc_html__( 'Show this video in about page template', 'dcf' ),
 					),
 				),
 			);
